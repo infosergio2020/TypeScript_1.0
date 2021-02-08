15b3e8f7a90bb1f268af228b270d96b47e2c7d7a
infosergio2020 <infosergio2020@gmail.com>
2021-02-08 18:39:05 -0300

    tipos primitivos FIN

---
+++ q
@@ -0,0 +1,11 @@
+[33mcommit 4ca80fe4a74faa971db66f9a34ff97c29e7f9944[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: infosergio2020 <infosergio2020@gmail.com>
+Date:   Sun Feb 7 22:34:50 2021 -0300
+
+    tipos primitivos, boolean, number
+
+[33mcommit 52686376ddaa837e72efb81716ff48f56a43244c[m
+Author: infosergio2020 <infosergio2020@gmail.com>
+Date:   Sun Feb 7 21:47:13 2021 -0300
+
+    se agrego un src un dist y un archivo tsconfig ademas de unos ejemplos de ECMASCRIPT en el archivo edteam.ts
--- src/clase-02/clase-02.ts
+++ src/clase-02/clase-02.ts
@@ -45,3 +45,100 @@ let octal = 0o755;
 console.log('octal',octal);
 
 
+//STRING
+
+let nombre = 'Sergio';
+let apellido: string = 'Aguilar';
+let nombreCompleto = nombre + ' ' + apellido;
+console.log('nombreCompleto', nombreCompleto);
+
+//String + ES6 Template literals
+let nombreCompleto2 = `${nombre} ${apellido}`;
+console.log('nombreCompleto2', nombreCompleto2);
+let arregloCursos = ['Typescript','Angular'];
+let mensaje = `
+Mi nombre es  ${nombreCompleto} y tengo ${arregloCursos.length} cursos.
+¡Gracias por asistir!
+`;
+let mensaje2 = '\n Mi nombre es ' + nombreCompleto + ' y tengo ' +arregloCursos.length + '\n ¡Gracias por asistir!'
+
+//los tipos especiales en Typescript
+// Tipo: any
+let desconocido;
+desconocido = 2;
+let desconocido2:any = 2;
+
+//Tipo: Void
+let vacio:void;
+function mostrarContenido(curso:string):void {
+    const mensaje = curso ? `Bienvenido al curso ${curso}`: 'Suscribete al curso!'
+    console.log('mensaje',mensaje);
+}
+mostrarContenido('typescript');
+
+//Tipo: Never
+
+let nunca:never;
+
+function retornaError(error:string): never {
+    throw new Error("Error TypeScript. "+ error);
+    //Nunca retorna un valor!
+}
+
+// retornaError('Valor inesperado');
+function cicloInfinito():never {
+    while (true) {}
+}
+
+//Tipo: Null y Undefined
+
+let variableUndefined: undefined = undefined;
+let variableNull: null = null;
+
+function retornoNull():null {
+    return null;
+}
+function retornoUndefined():undefined {
+    return undefined;
+}
+
+//Funciones en TypeScript
+
+function hola(nombre?:string): string {
+    if (nombre)
+        return 'Hola '+ nombre;
+    return '¡Hola!'
+}
+console.log(hola(nombreCompleto));
+console.log(hola());
+
+
+//Arreglos en Typescript
+let cursos123:string[];
+cursos123 = ['typescript','Angular'];
+//arreglos con generics
+let nombres: Array<string>
+nombres = ['Luis Alvares','Alvaro Felipe'];
+let arreglo: any[] = [2,'cadena',true]; //solo deberia usarse cuando interactuamos con librerias externas
+
+
+//Tuplas
+let infoCursos: [string,number] = ['typescript',100];
+infoCursos = ['angular',200];
+
+let curso1234: [string,number,string] = ['typescript',150,'08/2019']
+console.log('curso',curso1234);
+
+let nuevosInscritos = 10
+
+console.log('fecha de inicio',(curso1234[2]));
+console.log('Total de inscritos', curso1234[1] + nuevosInscritos);
+
+let tuplaTest: [string,string] = ['a','b'];
+let tuplaTemp: [string,string] = ['c','d'];
+
+tuplaTemp = tuplaTest;
+console.log('tuplaTemp',tuplaTemp);
+
+
+
